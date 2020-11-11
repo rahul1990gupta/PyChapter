8535d5c2326f40a75106204f1d2cae78c82098f6
Rahul Gupta <rahul1990gupta@gmail.com>
2020-11-11 06:49:53 +0530

    Register all models in the admin panel

--- chapter/admin.py
+++ chapter/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Chapter, Event, Person, Rsvp, Venue
+
+admin.site.site_header = "Chapter Admin"
+admin.site.site_title = "Chapter Admin Area"
+admin.site.index_title = "Welcome to the Chapter admin area"
+
+admin.site.register(Chapter)
+admin.site.register(Event)
+admin.site.register(Person)
+admin.site.register(Rsvp)
+admin.site.register(Venue)
